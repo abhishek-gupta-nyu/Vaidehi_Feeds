d0b2634bc7d7f163f3a2981c46884a2f25413773
Abhishek Gupta <ab.smart.007@gmail.com>
2015-07-20 23:03:52 +0530

    Added volley,jar to libs folder. Right click on volley.jar and add as library
    Made a new package app, and two new classes AppConfig, AppController
    
    http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/

---
+++ app/src/main/java/vaidehifeeds/com/vaidehifeeds/app/AppConfig.java
@@ -0,0 +1,12 @@
+package vaidehifeeds.com.vaidehifeeds.app;
+
+/**
+ * Created by BB3 on 7/20/2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.101/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.101/android_login_api/";
+}
