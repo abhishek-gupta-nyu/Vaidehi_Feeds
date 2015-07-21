b7993966796464de8271308aaa0aefae79fc4ede
Abhishek Gupta <ab.smart.007@gmail.com>
2015-07-21 13:38:42 +0530

    Login and registeration complete

--- app/src/main/java/vaidehifeeds/com/vaidehifeeds/MainActivityFragment.java
+++ app/src/main/java/vaidehifeeds/com/vaidehifeeds/MainActivityFragment.java
@@ -1,10 +1,18 @@
 package vaidehifeeds.com.vaidehifeeds;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import vaidehifeeds.com.vaidehifeeds.helper.SQLiteHandler;
+import vaidehifeeds.com.vaidehifeeds.helper.SessionManager;
 
 
 /**
@@ -12,12 +20,66 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+
+        txtName = (TextView) rootview.findViewById(R.id.name);
+        txtEmail = (TextView) rootview.findViewById(R.id.email);
+        btnLogout = (Button) rootview.findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getActivity().getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getActivity().getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+        return rootview;
+
+    }
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
