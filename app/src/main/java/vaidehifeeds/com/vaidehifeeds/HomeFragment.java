2afb40feeb91164dc3e88d9cbc5290e825f6a650
Abhishek Gupta <ab.smart.007@gmail.com>
2015-07-22 14:42:18 +0530

    Nav Drawer
    About Page updated

--- app/src/main/java/vaidehifeeds/com/vaidehifeeds/HomeFragment.java
+++ app/src/main/java/vaidehifeeds/com/vaidehifeeds/HomeFragment.java
@@ -38,7 +38,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_home, container, false);
 
         txtName = (TextView) rootview.findViewById(R.id.name);
         txtEmail = (TextView) rootview.findViewById(R.id.email);
